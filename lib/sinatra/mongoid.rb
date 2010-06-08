300dc85b76e383f523ee904904ebd952e4bd122b
Sean Porter <portertech@gmail.com>
2010-06-08 01:06:49 -0700

    updated env vars to match the mongoid documentation

--- lib/sinatra/mongoid.rb
+++ lib/sinatra/mongoid.rb
@@ -4,11 +4,11 @@ require 'mongoid'
 module Sinatra
   module MongoidExtension
     def self.registered(app)
-      app.set :mongo_host,     ENV['MONGO_HOST'] || 'localhost'
-      app.set :mongo_db,       ENV['MONGO_DB']   || 'changeme'
-      app.set :mongo_port,     ENV['MONGO_PORT'] || Mongo::Connection::DEFAULT_PORT
-      app.set :mongo_user,     ENV['MONGO_USER']
-      app.set :mongo_password, ENV['MONGO_PASSWORD']
+      app.set :mongo_host,     ENV['MONGOID_HOST']      || 'localhost'
+      app.set :mongo_db,       ENV['MONGOID_DATABASE']  || 'changeme'
+      app.set :mongo_port,     ENV['MONGOID_PORT']      || Mongo::Connection::DEFAULT_PORT
+      app.set :mongo_user,     ENV['MONGOID_USERNAME']
+      app.set :mongo_password, ENV['MONGOID_PASSWORD']
 
       Mongoid.database = Mongo::Connection.new(app.mongo_host,app.mongo_port).
                                            db(app.mongo_db)
